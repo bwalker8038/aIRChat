e98e787377e62fc77f40ca06750530592d5a196b
redwire <redwire@riseup.net>
2014-03-06 20:40:26 -0330

    Configuration file necessary for connecting to a database and accessing resources

---
+++ config.js
@@ -0,0 +1,7 @@
+/** Database Configuration
+  * Used to build the URL to access the postgres database, form:
+  * postgres://<username>:<password>@<location>
+  */
+exports.username = '';
+exports.password = '';
+exports.location = 'localhost/postgres';
