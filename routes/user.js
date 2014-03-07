64b0044f419fb625ec9137ff63d770b983008913
redwire <redwire@riseup.net>
2014-03-07 20:06:42 -0330

    Wrote beginning of code to handle authentication and access privilege

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,52 @@
   * to the chat page to test all the important stuff.
   */
 
+var authenticates = function (user, pass) {
+  return true;
+};
+
+var register = function (user, pass, passr) {
+  return true;
+};
+
 exports.login = function (req, res) {
-  res.redirect('/chat');
+  if (req.route.method === 'get') {
+    if (req.session.loggedIn === true) {
+      res.redirect('/chat');
+    } else {
+      res.redirect(401, '/');
+    }
+  } else if (req.route.method === 'post') {
+    if (authenticates(req.body.username, req.body.password) === true) {
+      req.session.loggedIn = true;
+      req.session.username = req.body.username;
+      res.redirect('/chat');
+    } else {
+      res.redirect(401, '/');
+    }
+  } else {
+    res.redirect(400, '/');
+  }
 };
 
 exports.register = function (req, res) {
-  res.redirect('/chat');
+  if (req.route.method === 'get') {
+    if (req.session.loggedIn === true) {
+      res.redirect('/chat');
+    } else {
+      res.redirect(400, '/'); 
+    }
+  } else if (req.route.method === 'post') {
+    if (register(
+          req.body.username, req.body.password, req.body.passwordrepeat
+        ) === true) {
+      req.session.loggedIn = true;
+      req.session.username = req.body.username;
+      res.redirect('/chat');
+    } else {
+      res.redirect(500, '/'); // Use internal server error for reg. failute.
+    }
+  } else {
+    res.redirect(400, '/'); 
+  }
 };
