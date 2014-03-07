a9a99c94364ecda9a151b2374bd04bf916f3d234
redwire <redwire@riseup.net>
2014-03-07 00:25:16 -0330

    Writing some test code to get communication to the server from the client working

---
+++ routes/chat.js
@@ -0,0 +1,27 @@
+var irc = require('irc');
+
+var clients = new Array();
+
+// Array remove - By John Resig (MIT LICENSED)
+Array.prototype.remove = function (start, end) {
+  var tail = this.slice((end || start) + 1 || this.length);
+  this.length = start < 0 ? this.length + start : start;
+  return this.push.apply(this, tail);
+};
+
+exports.addClient = function (socket) {
+  clients.push(socket);
+  console.log('New connection.');
+
+  socket.on('message', function (message, callback) {
+    console.log("received: " + message);
+  });
+  
+  socket.on('disconnect', function () {
+    clients.remove(clients.indexOf(socket));
+  });
+};
+
+exports.main = function (req, res) {
+  res.render('chat', {title: 'aIRChat'});
+};
