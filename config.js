cfca322b440bb1eea739279d31f6976375d45d88
redwire <redwire@riseup.net>
2014-04-14 14:12:26 -0230

    Remove Postgres configuration information since MongoDB is being used instead

--- config.js
+++ config.js
@@ -1,11 +1,3 @@
-/** Database Configuration
-  * Used to build the URL to access the postgres database, form:
-  * postgres://<username>:<password>@<location>
-  */
-exports.username = '';
-exports.password = '';
-exports.location = 'localhost/postgres';
-
 /** Client Configuration
   * Used to configure information the client needs to create a socket.io
   * connection to the server
