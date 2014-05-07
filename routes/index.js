4e9e77f35f70da198b8da7db0073ff2500f7c8b2
redwire <redwire@riseup.net>
2014-05-07 19:18:02 -0230

    Removed unused password testing and needless logging

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 var config = require('../config');
 
 exports.index = function(req, res) {
-  console.log('Got query:');
-  console.log(req.query);
   var ajServer = req.query.server;
   var ajChannel = req.query.channel;
-  console.log('ajServer = ' + ajServer);
-  console.log('ajChannel = ' + ajChannel);
   res.render('index', {
     title              : 'aIRChat',
     showDonationButton : config.showDonationButton,
