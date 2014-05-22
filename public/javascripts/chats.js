85d8683e6b757d63a5e018f9b1a590e0520752e0
redwire <redwire@riseup.net>
2014-05-22 20:19:03 -0230

    Removed unneeded notification count in chats

--- public/javascripts/chats.js
+++ public/javascripts/chats.js
@@ -6,37 +6,10 @@ var Chat = function (server, channel) {
   this.server = server;
   this.channel = channel;
   this.users = new Array();
-  this.notifications = {low: 0, high: 0};
 };
 
 Chat.prototype.sameChat = function (server, channel) {
   return this.server === server && this.channel.toLowerCase() === channel.toLowerCase();
-};
-
-Chat.prototype.getUser = function (nick) {
-  var index = userIndex(this.users, nick);
-  return this.users[index];
-};
-
-Chat.prototype.gotLowPriorityMessage = function () {
-  this.notifications.low++;
-};
-
-Chat.prototype.gotHighPriorityMessage = function () {
-  this.notifications.high++;
-};
-
-Chat.prototype.lowPriorityNotifications = function () {
-  return this.notifications.low;
-};
-
-Chat.prototype.highPriorityNotifications = function () {
-  return this.notifications.high;
-};
-
-Chat.prototype.clearNotifications = function () {
-  this.notifications.low = 0;
-  this.notifications.high = 0;
 };
 
 function chatIndex(chatList, server, channel) {
