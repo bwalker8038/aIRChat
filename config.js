5e0bcf50b7b9ea439623495136bbc5e205b28c25
redwire <redwire@riseup.net>
2014-06-02 19:11:39 -0230

    Remove clone

--- config.js
+++
@@ -1,5 +0,0 @@
-/** Client Configuration
-  * Used to configure information the client needs to create a socket.io
-  * connection to the server
-  */
-exports.host = 'http://localhost';
--- public/javascripts/main.js
+++
@@ -1,2 +0,0 @@
-// Initialize Foundation javascript.
-$(document).foundation();
